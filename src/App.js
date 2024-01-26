78972c581dcb5b8e46852a80c8c92655acb606a7
taskger <chayanon.poo@ku.th>
2024-01-26 21:11:38 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Login from './components/Login/Login';
+import Home from './components/Admin/Home/home';
+import Schedule from './components/Admin/Schedule/schedule';
+import Course from './components/Admin/Course/course';
+import Registration from './components/Admin/Registration/registration';
+import Confirm from './components/Admin/Confirm/confirm';
+import Userhome from './components/User/Home/home';
+import Userschedule from './components/User/Schedule/schedule';
+import Useraddcourse from './components/User/Addcourse/addcourse';
 
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/admin/home' element={<Home/>}></Route>
+      <Route path='/admin/schedule' element={<Schedule/>}></Route>
+      <Route path='/admin/course' element={<Course/>}></Route>
+      <Route path='/admin/registration' element={<Registration/>}></Route>
+      <Route path='/admin/confirm' element={<Confirm/>}></Route>
+      <Route path='/user/home' element={<Userhome/>}></Route>
+      <Route path='/user/schedule' element={<Userschedule/>}></Route>
+      <Route path='/user/addcourse' element={<Useraddcourse/>}></Route>
+    </Routes>
 
-export default App;
+    </BrowserRouter>
+
+  )
+}
+export default App
